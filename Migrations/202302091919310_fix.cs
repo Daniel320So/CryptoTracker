5f37765ffe1db41c7c506452b85a9f567d4caca7
Daniel So Ho Kit <58765171+Daniel320So@users.noreply.github.com>
2023-02-09 14:20:44 -0500

    Fix migration issue

---
+++ Migrations/202302091919310_fix.cs
@@ -0,0 +1,36 @@
+﻿namespace CryptoTracker.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class fix : DbMigration
+    {
+        public override void Up()
+        {
+            //DropTable("dbo.Samples");
+            //DropTable("dbo.TokenBalances");
+        }
+        
+        public override void Down()
+        {
+            CreateTable(
+                "dbo.TokenBalances",
+                c => new
+                    {
+                        TokenBalanceId = c.Int(nullable: false, identity: true),
+                        Balance = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.TokenBalanceId);
+            
+            CreateTable(
+                "dbo.Samples",
+                c => new
+                    {
+                        TokenBalanceId = c.Int(nullable: false, identity: true),
+                        Balance = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.TokenBalanceId);
+            
+        }
+    }
+}
